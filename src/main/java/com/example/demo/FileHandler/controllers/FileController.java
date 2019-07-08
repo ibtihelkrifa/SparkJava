ca499547d7b9c1c6862efc4c1ac6393d87a1929a
ibtihel <krifa.ibtihel@gmail.com>
2019-07-08 15:17:39 +0100

    function map that counts the number of columns in each csv file to verify the file integrity

--- src/main/java/com/example/demo/FileHandler/controllers/FileController.java
+++ src/main/java/com/example/demo/FileHandler/controllers/FileController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class FileController {
 
@@ -20,6 +22,13 @@ public class FileController {
        return this.fileService.getNumberWords();
     }
 
+
+    @GetMapping("/getNumberColumns")
+    public List<Integer> numberOfColumnsInEachLine()
+    {
+        return this.fileService.getNumberColumns();
+
+    }
 
 
 
--- src/main/java/com/example/demo/FileHandler/services/FileService.java
+++ src/main/java/com/example/demo/FileHandler/services/FileService.java
@@ -6,12 +6,14 @@ import com.google.common.io.Resources;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.SparkSession;
 import org.springframework.stereotype.Service;
 import scala.Serializable;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 @Service
 public class FileService  implements Serializable{
@@ -40,5 +42,16 @@ public class FileService  implements Serializable{
         return words.collect().size();
 
     }
+
+    public List<Integer> getNumberColumns()
+    {
+        JavaRDD<String> inputFIle= sc.textFile(Resources.getResource("Files/policy.csv").getPath());
+        JavaRDD<Integer> linesSize= inputFIle.map(s -> s.split(";").length );
+        
+        return linesSize.collect();
+
+    }
+
+
 
 }
